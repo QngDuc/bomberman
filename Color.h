bec04baa24e963a6ce86609ba948d0271a497e30
HuyHoangI4t <152394866+HuyHoangI4t@users.noreply.github.com>
2024-11-22 16:09:25 +0700

    bom

--- Color.h
+++ Color.h
@@ -3,21 +3,18 @@
 class Color
 {
 public:
-    // Constructor
     Color(CDC* dc);
-
-    // Các phương thức để vẽ các thành phần với màu sắc
+    
     void DrawCell(const CRect& rect, int colorType);
     void SetColor(int colorType);
 
-private:
-    CDC* dc;  // CDC dùng để vẽ
-    CBrush wallBrush;    // Màu đen
-    CBrush pathBrush;    // Màu trắng
-    CBrush obstacleBrush;      // Màu đỏ
+    CDC* dc;  
+    CBrush wallBrush;    
+    CBrush pathBrush;    
+    CBrush obstacleBrush;      
     CBrush playerBrush;
 
-    // Helper method để thiết lập brush theo loại màu
+    
     CBrush* GetColor(int colorType);
     CBrush* playerColor(int colorType);
 };
